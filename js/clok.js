f3764fd6713a71de26900186361c0359ecba8245
Bahar Karaduman <137704861+baharkaraduman@users.noreply.github.com>
2024-12-24 20:29:35 +0300

    Update clok.js

--- js/clok.js
+++ js/clok.js
@@ -1,6 +1,6 @@
 function askName() {
-  let name = prompt("Lütfen Adınızı Giriniz.");
-  document.getElementById("myName").inner.Text = name;
+  let name = prompt("Lütfen Adinizi Giriniz.");
+  document.getElementById("myName").innerText = name;
 }
 
 function showTime() {
@@ -8,22 +8,22 @@ function showTime() {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-  let weekDayNumber = date.getDay();
+  let dayNumber = date.getDay();
 
 
-  let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"]
+  let days = ["Pazar", "Pazartesi", "Sali", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"]
   let daysName = days[dayNumber]
 
    hours = hours < 10 ? "0" + hours : hours;
    minutes = minutes < 10 ? "0" + minutes : minutes;
-   seconds = seconds < 10 ? "0" + second : seconds;
+   seconds = seconds < 10 ? "0" + seconds : seconds;
 
 
-  let time = hours + ":" + m + ":" + s + " " + daysName;
+  let time = hours + ":" + minutes + ":" + seconds + " " + daysName;
   document.getElementById("myClock").innerText = time;
   document.getElementById("myClock").textContent = time; 
 
-  setTimeOut(showTime, 1000);
+  setTimeout(showTime, 1000);
 
 }
 
